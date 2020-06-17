888b7b48aa84bc6102c6bb451068d171b8117173
pankaj <pankajthekush@gmail.com>
2020-06-17 13:54:42 +0530

    udpate

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup,find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+from glob import glob
+import os
+import sys
+current_path  = os.path.dirname(os.path.realpath(__file__))
+
+setup(
+    name='wgetter',
+    version='1.0',
+    # cmdclass={'develop': PostDevelopCommand,'install': PostInstallCommand},
+    packages=['gscrape'],
+    entry_points ={'console_scripts': ['wgetter = wgetter.wgetter:threaded_wget']}
+)
